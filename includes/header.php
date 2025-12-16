53853eaf47697f3244972c1d4358eee85a0e9b5a
Dazz <120923800+d44zzyy@users.noreply.github.com>
2025-12-16 10:16:30 +0100

    Fix image source URL in header.php

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
 <header>
     <title> Weebshop </title>
 </header>
-<img src="https://u.cubeupload.com/123456wdw/IMG0211.png" alt="weebshop logo">
+<img src="https://u.cubeupload.com/123456wdw/https://u.cubeupload.com/123456wdw/weebshop.png" alt="weebshop logo">
 </div>
 
 <link rel="stylesheet" type="text/css" href="style.css">
